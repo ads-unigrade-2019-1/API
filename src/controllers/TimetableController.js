cea467d6350cb5824d23914a8da4db0fb1d1f5a2
Joberth Rogers <joberth.rogers18@gmail.com>
2019-05-10 22:59:05 -0400

    Created an object list with timetable default values return

--- src/controllers/TimetableController.js
+++ src/controllers/TimetableController.js
@@ -6,7 +6,75 @@ const object = `[
                 {"name" : "B", "vacancies" : 1, "discipline" : "101095", "meetings" : [ { "day" : "Terça", "init_hour" : "16:00", "final_hour" : "17:50", "room" : "FGA-I7" }, { "day" : "Quinta", "init_hour" : "16:00", "final_hour" : "17:50", "room" : "FGA-S8" } ], "shift" : "Diurno", "teachers" : [ "BRUNO CÉSAR RIBAS" ], "campus" : 4,  "priority": 1 }
                  ]`
 
+const timetableDefault = [
+    { "name": "", "day": "Segunda", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "06:00", "final_hour": "07:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "08:00", "final_hour": "09:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "10:00", "final_hour": "11:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "12:00", "final_hour": "13:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "14:00", "final_hour": "15:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "16:00", "final_hour": "17:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "18:00", "final_hour": "19:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "20:00", "final_hour": "21:50", "room": "" },
+    
+    { "name": "", "day": "Segunda", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+    { "name": "", "day": "Terça", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+    { "name": "", "day": "Quarta", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+    { "name": "", "day": "Quinta", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+    { "name": "", "day": "Sexta", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+    { "name": "", "day": "Sabado", "init_hour": "22:00", "final_hour": "23:50", "room": "" },
+
+]
+
 function parseObjects(timetableJson) {
+
+    console.log(timetableDefault.length)
 
     let timetableObject = JSON.parse(timetableJson)
     timetableObject.sort((classA, classB) => (classA.priority > classB.priority) ? 1 : -1)
@@ -16,6 +84,8 @@ function parseObjects(timetableJson) {
 
 module.exports = {
     mountTimetable(req, res) {
-        res.json(parseObjects(object))
+        parseObjects(object)
+
+        res.json(timetableDefault)
     }
 }
