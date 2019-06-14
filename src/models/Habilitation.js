7901d1f54f32088b6cf3162066cbaac16ed3bea8
Guilherme Deusdará <guibanci@gmail.com>
2019-06-13 23:57:35 -0300

    reorganizing pastes
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    
    Co-authored-by: Ezequel <ezequiel1de1oliveira@gmail.com>

--- src/models/Habilitation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const HabilitationSchema = new mongoose.Schema({
-    code : String,
-    name : String,
-    disciplines : [[[String]]]
-});
-
-module.exports = mongoose.model('Habilitation', HabilitationSchema);
